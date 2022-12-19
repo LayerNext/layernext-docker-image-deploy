990367f7f8e5ee54a873fc0f5ab2376ac40a86d6
Chathushka <89377148+chathushkaTzoomi@users.noreply.github.com>
2022-12-19 22:00:38 +0530

    process

--- accounts/crontab.sh
+++ accounts/crontab.sh
@@ -1,6 +1,5 @@
-
-PATH_ENV = realpath .env
-PATH_DIR = pwd
+PATH_ENV=$(realpath .env)
+PATH_DIR=$(realpath ..)
 #change the path to dump.py
 
-(crontab -l 2>/dev/null; echo "* * * * * python3 $PATH_DIR/dump.py $PATH_ENV") | crontab -
+(crontab -l 2>/dev/null; echo "0 * * * * python3 $PATH_DIR/backup/dump.py $PATH_ENV $PATH_DIR") | crontab -
