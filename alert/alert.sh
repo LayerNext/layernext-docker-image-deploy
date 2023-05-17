3690d8f05c3538efae2316a5716fa55d852bc882
Chathushka <89377148+chathushkaTzoomi@users.noreply.github.com>
2023-05-17 09:33:54 +0530

    Update alert.sh

--- alert/alert.sh
+++ alert/alert.sh
@@ -1,4 +1,4 @@
 
 PATH_DIR=$(realpath .)
 
-(crontab -l 2>/dev/null; echo "*/1 * * * * python3 $PATH_DIR/alert.py  >>  $PATH_DIR/output.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * python3 $PATH_DIR/alert.py  >>  $PATH_DIR/output.log 2>&1") | crontab -
