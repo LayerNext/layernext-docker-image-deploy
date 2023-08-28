e09aaf74cfb0fc1ecbf18150b98bcdb56224b5c8
channa-zoomi <102577454+channa-zoomi@users.noreply.github.com>
2023-08-28 13:30:23 +0530

    Update create-index.js

--- datalake/scripts/1.0.21/create-index.js
+++ datalake/scripts/1.0.21/create-index.js
@@ -1,2 +1,3 @@
+db.getCollection('MetaData').dropIndex("datasetVersionList.datasetVersionId_1_collectionId_1")
 db.getCollection('MetaData').createIndex({"bucketName": 1})
 db.getCollection('MetaData').createIndex({"storagePath": 1})
