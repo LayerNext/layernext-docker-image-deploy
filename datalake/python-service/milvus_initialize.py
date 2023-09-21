a262ce866cb8411de3460dca51ebc00427db3ddc
Chathushka <chathushka@zoomi.ca>
2023-09-21 18:03:29 +0530

    Update milvus_initialize.py

--- datalake/python-service/milvus_initialize.py
+++ datalake/python-service/milvus_initialize.py
@@ -15,35 +15,35 @@ db_list = db.list_database()
 if "LayerNext" not in db_list:
     database = db.create_database("LayerNext")
 
-    db.using_database("LayerNext")
+db.using_database("LayerNext")
 
-    fields = [
-        FieldSchema(name="uniqueName", dtype=DataType.VARCHAR, is_primary=True, auto_id=False, max_length=256),
-        FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=2048)
-    ]
+fields = [
+    FieldSchema(name="uniqueName", dtype=DataType.VARCHAR, is_primary=True, auto_id=False, max_length=256),
+    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=2048)
+]
 
-    schema = CollectionSchema(fields, "LayerNext embeddings")
+schema = CollectionSchema(fields, "LayerNext embeddings")
 
-    LayerNextEmbeddings = Collection("LayerNextEmbeddings", schema, consistency_level="Strong")
+LayerNextEmbeddings = Collection("LayerNextEmbeddings", schema, consistency_level="Strong")
 
-    LayerNextEmbeddings.insert(
-    [
-        {
-        "uniqueName": "test",
-        "embeddings": np.random.rand(2048).astype(np.float32)
-        }
-    ]
-    )
-
-    index = {
-        "index_type": "IVF_FLAT",
-        "metric_type": "IP",
-        "params": {"nlist": 128},
+LayerNextEmbeddings.insert(
+[
+    {
+    "uniqueName": "test",
+    "embeddings": np.random.rand(2048).astype(np.float32)
     }
+]
+)
 
-    LayerNextEmbeddings = Collection("LayerNextEmbeddings")
+index = {
+    "index_type": "IVF_FLAT",
+    "metric_type": "IP",
+    "params": {"nlist": 128},
+}
 
-    LayerNextEmbeddings.create_index("embeddings", index, index_name="vec_index")
+LayerNextEmbeddings = Collection("LayerNextEmbeddings")
+
+LayerNextEmbeddings.create_index("embeddings", index, index_name="vec_index")
 
 
-    time.sleep(20000)
+time.sleep(20000)
