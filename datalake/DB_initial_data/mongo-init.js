d0ffcbedd337dc20acaba1aca7e3a61b8ff54fb3
Chathushka <chathushka@zoomi.ca>
2023-11-01 15:46:09 +0530

    Update mongo-init.js

--- datalake/DB_initial_data/mongo-init.js
+++ datalake/DB_initial_data/mongo-init.js
@@ -327,3 +327,63 @@ db.Job.createIndex({ jobType: 1 });
 db.Job.createIndex({ updatedAt: -1 });
 db.Job.createIndex({ status: 1 });
 db.Job.createIndex({ createdAt: 1 });
+
+
+db.getCollection('EmbeddingVector').createIndex(
+  {
+    objectKey: 1.0,
+    modelName: 1.0,
+  },
+  {
+    unique: true,
+    name: 'objectKey_1_modelName_1',
+  },
+);
+
+db.getCollection('SimilarImage').createIndex(
+  {
+    objectKey: 1,
+  },
+  {
+    name: 'objectKey_1',
+  },
+);
+db.getCollection('SimilarImage').createIndex(
+  {
+    referenceObjectKey: 1,
+    objectKey: 1,
+    modelName: 1,
+    scoreThreshold: 1,
+    score: -1,
+  },
+  {
+    name: 'search',
+  },
+);
+
+db.getCollection('QueryGraphData').createIndex(
+  {
+    graphId: 1.0,
+    objectKey: 1.0,
+  },
+  {
+    name: 'graphId_1_objectKey_1',
+  },
+);
+db.getCollection('QueryGraphData').createIndex(
+  {
+    coordinates: '2d',
+  },
+  {
+    name: 'coordinates_2d',
+  },
+);
+
+db.getCollection('MetaData').createIndex(
+  {objectKey: 1},
+  {
+    unique: true,
+    partialFilterExpression: {objectKey: {$exists: true}},
+  },
+);
+
