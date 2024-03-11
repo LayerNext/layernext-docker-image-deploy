4542d050bb5d42176d6323f4938ad68a2df9e938
Chathushka <chathushka@zoomi.ca>
2024-03-11 14:43:27 +0530

    changes

--- accounts/DB_initial_data/mongo-init.js
+++ accounts/DB_initial_data/mongo-init.js
@@ -1,118 +1,87 @@
 // create mongodb dump restore user
-db.createUser(
-  {
-    user: _getEnv('DUMP_USER'),
-    pwd: _getEnv('DUMP_USER_PWD'),
-    roles: [{role: "backup", db: "admin"}, {role: "restore", db: "admin"}],
-    mechanisms: ["SCRAM-SHA-1"]
-  }
-);
+db.createUser({
+  user: _getEnv("DUMP_USER"),
+  pwd: _getEnv("DUMP_USER_PWD"),
+  roles: [
+    { role: "backup", db: "admin" },
+    { role: "restore", db: "admin" },
+  ],
+  mechanisms: ["SCRAM-SHA-1"],
+});
 
 // switch to db
-db = db.getSiblingDB(_getEnv('DATABASE'));
+db = db.getSiblingDB(_getEnv("DATABASE"));
 
 // create mongodb user
-db.createUser(
-  {
-    user: _getEnv('DB_USER'),
-    pwd: _getEnv('DB_PASS'),
-    roles: [{role: "readWrite", db: _getEnv('DATABASE')}],
-    mechanisms: ["SCRAM-SHA-1"]
-  }
-);
+db.createUser({
+  user: _getEnv("DB_USER"),
+  pwd: _getEnv("DB_PASS"),
+  roles: [{ role: "readWrite", db: _getEnv("DATABASE") }],
+  mechanisms: ["SCRAM-SHA-1"],
+});
 
-var SETUP_CUSTOMER = _getEnv('SETUP_CUSTOMER')
+var SETUP_CUSTOMER = _getEnv("SETUP_CUSTOMER");
 
-var adminMail = _getEnv('ADMIN_EMAIL')
-var teamName = `${SETUP_CUSTOMER} team`
-var imageUrl = `https://accounts.${SETUP_CUSTOMER}.layernext.ai/api/user/profileImage/6374c47ecb468b7a7a68a117/defaultProfileImage.png?1669197094012`
+var adminMail = _getEnv("ADMIN_EMAIL");
+var teamName = `${SETUP_CUSTOMER} team`;
+var imageUrl = `https://accounts.${SETUP_CUSTOMER}.layernext.ai/api/user/profileImage/6374c47ecb468b7a7a68a117/defaultProfileImage.png?1669197094012`;
 
 //insert default user
-db.getCollection('User').insert({
-  "_id": ObjectId("6374c47ecb468b7a7a68a117"),
-  "email": adminMail,
-  "name": "LayerNext Admin",
-  "userType": 3,
-  "profileImgUrl": "defaultProfileImage.png",
-  "projectList": null,
-  "teamId": ObjectId(_getEnv('TEAM_ID')),
-  "teamName": teamName,
-  "userStatus": 1,
-  "isAll": false,
-  "inviteUrlCreatedAt": null,
-  "isUserDeactivated": false,
-  "lastEmailSentTime": null,
-  "userStatPending": false,
-  "isBillingAdmin": false,
-  "annotationStats": null,
-  "createdAt": null,
-  "imageUrl": imageUrl,
-  "offsetTime": null,
-  "taskStats": null,
-  "timeZoneOffset": null
-})
+db.getCollection("User").insert({
+  _id: ObjectId("6374c47ecb468b7a7a68a117"),
+  email: adminMail,
+  name: "LayerNext Admin",
+  userType: 3,
+  profileImgUrl: "defaultProfileImage.png",
+  projectList: null,
+  teamId: ObjectId(_getEnv("TEAM_ID")),
+  teamName: teamName,
+  userStatus: 1,
+  isAll: false,
+  inviteUrlCreatedAt: null,
+  isUserDeactivated: false,
+  lastEmailSentTime: null,
+  userStatPending: false,
+  isBillingAdmin: false,
+  annotationStats: null,
+  createdAt: null,
+  imageUrl: imageUrl,
+  offsetTime: null,
+  taskStats: null,
+  timeZoneOffset: null,
+});
 //insert default user credentials
-db.getCollection('AppUserCredentials').insert({
-  "_id": ObjectId("6374c597cb468b7a7a68a118"),
-  "password": _getEnv('ADMIN_PASSWORD'),
-  "userId": ObjectId("6374c47ecb468b7a7a68a117"),
-  "annotationUserId": ObjectId("6374c47ecb468b7a7a68a117")
-})
+db.getCollection("AppUserCredentials").insert({
+  _id: ObjectId("6374c597cb468b7a7a68a118"),
+  password: _getEnv("ADMIN_PASSWORD"),
+  userId: ObjectId("6374c47ecb468b7a7a68a117"),
+  annotationUserId: ObjectId("6374c47ecb468b7a7a68a117"),
+});
 
 //insert default team
-db.getCollection('AnnotationTeam').insert({
-  "_id": ObjectId(_getEnv('TEAM_ID')),
-  "teamName": teamName,
-  "create_team_folder": true,
-  "apiKeyGenerated": true
-})
-
-//insert analytics APIKey
-db.getCollection('ApiKey').insert({
-  "_id": ObjectId("6374eb51e3ac085579e53443"),
-  "name": "Automatic Analysis",
-  "teamId": ObjectId(_getEnv('TEAM_ID')),
-  "key": _getEnv('ANALYTICS_KEY'),
-  "secret": _getEnv('ANALYTICS_SECRET'),
-  "type": 2
-})
-
-//insert dataset APIKey
-db.getCollection('ApiKey').insert({
-  "_id": ObjectId("6374eb51e3ac085579e53445"),
-  "name": "Dataset Manager",
-  "teamId": ObjectId(_getEnv('TEAM_ID')),
-  "key": _getEnv('DATASET_KEY'),
-  "secret": _getEnv('DATASET_SECRET'),
-  "type": 2
-})
-
-//insert studio APIKey
-db.getCollection('ApiKey').insert({
-  "_id": ObjectId("6374eb51e3ac085579e53446"),
-  "name": "Annotation Studio",
-  "teamId": ObjectId(_getEnv('TEAM_ID')),
-  "key": _getEnv('STUDIO_KEY'),
-  "secret": _getEnv('STUDIO_SECRET'),
-  "type": 2
-})
+db.getCollection("AnnotationTeam").insert({
+  _id: ObjectId(_getEnv("TEAM_ID")),
+  teamName: teamName,
+  create_team_folder: true,
+  apiKeyGenerated: true,
+});
 
 //insert datalake APIKey
-db.getCollection('ApiKey').insert({
-  "_id": ObjectId("6374eb51e3ac085579e53447"),
-  "name": "Metalake",
-  "teamId": ObjectId(_getEnv('TEAM_ID')),
-  "key": _getEnv('DATALAKE_KEY'),
-  "secret": _getEnv('DATALAKE_SECRET'),
-  "type": 2
-})
+db.getCollection("ApiKey").insert({
+  _id: ObjectId("6374eb51e3ac085579e53447"),
+  name: "Metalake",
+  teamId: ObjectId(_getEnv("TEAM_ID")),
+  key: _getEnv("DATALAKE_KEY"),
+  secret: _getEnv("DATALAKE_SECRET"),
+  type: 2,
+});
 
 //insert chat APIKey
-db.getCollection('ApiKey').insert({
-  "_id": ObjectId("6374eb51e3ac085579e53448"),
-  "name": "Chat App",
-  "teamId": ObjectId(_getEnv('TEAM_ID')),
-  "key": _getEnv('CHAT_KEY'),
-  "secret": _getEnv('CHAT_SECRET'),
-  "type": 2
-})
+db.getCollection("ApiKey").insert({
+  _id: ObjectId("6374eb51e3ac085579e53448"),
+  name: "Chat App",
+  teamId: ObjectId(_getEnv("TEAM_ID")),
+  key: _getEnv("CHAT_KEY"),
+  secret: _getEnv("CHAT_SECRET"),
+  type: 2,
+});
--- generate-env.sh
+++ generate-env.sh
@@ -2,12 +2,7 @@
 
 export $(grep -v '^#' .env | xargs -d '\n')
 JWT_SECRET=$(openssl rand -hex 32)
-STUDIO_KEY=key_$(openssl rand -base64 60 | tr -dc 'a-z0-9' | head -c 32)
-STUDIO_SECRET=$(openssl rand -base64 45 | tr -dc 'a-z0-9' | head -c 20)
-DATASET_KEY=key_$(openssl rand -base64 60 | tr -dc 'a-z0-9' | head -c 32)
-DATASET_SECRET=$(openssl rand -base64 45 | tr -dc 'a-z0-9' | head -c 20)
-ANALYTICS_KEY=key_$(openssl rand -base64 60 | tr -dc 'a-z0-9' | head -c 32)
-ANALYTICS_SECRET=$(openssl rand -base64 45 | tr -dc 'a-z0-9' | head -c 20)
+
 DATALAKE_KEY=key_$(openssl rand -base64 60 | tr -dc 'a-z0-9' | head -c 32)
 DATALAKE_SECRET=$(openssl rand -base64 45 | tr -dc 'a-z0-9' | head -c 20)
 CHAT_KEY=key_$(openssl rand -base64 60 | tr -dc 'a-z0-9' | head -c 32)
@@ -198,195 +193,8 @@ MYSQL_PASSWORD=$(openssl rand -hex 10)
 EOL
 
 
-# generate dataset env
-dataset_env="./dataset/.env"
-if [ -f $dataset_env ]; then
-  echo "Existing env file found for dataset. replacing..."
-  echo "Warning: If the system was already built, the system may become non functional due to regeneration of db passwords"
-fi
-cat > $dataset_env <<EOL
-#DB
-DB_HOST=host.docker.internal
-DB_PORT=47017
-DB_USER=dataset_user
-DB_PASS=$(openssl rand -hex 10)
-DATABASE=datasetDB
-
-#DUMP
-DUMP_USER=layernext_dumprestoreuser
-DUMP_USER_PWD=$(openssl rand -hex 10)
-OUTPUT_DIRECTORY=dataset
-#change dump keepeing days as prefer and use dump per day like 1,2,3,4,6,12,24
-DUMP_KEEPING_DAYS=30
-DUMP_PER_DAY=4
-
-# Build time
-SETUP_CUSTOMER=$SETUP_CUSTOMER
-MONGODB_ADMIN_PASSWORD=$(openssl rand -hex 16)
-CPU_LIMIT=$CPU_LIMIT
-MEMORY_LIMIT=$MEMORY_LIMIT
-
-#env
-PORT=4000
-TEAM_ID=6374c3decb468b7a7a68a116
-
-JWT_SECRET=$JWT_SECRET
-
-BASE_URL=https://dataset.$SETUP_CUSTOMER.layernext.ai
-API_URL=https://api.$SETUP_CUSTOMER.layernext.ai
-FRONT_END_BASE_URL=http://dataset.$SETUP_CUSTOMER.layernext.ai
-
-SSO_INTERNAL_SERVER=http://host.docker.internal:8888
-ANNO_INTERNAL_SERVER=http://host.docker.internal:8080
-DATALAKE_INTERNAL_SERVER=http://host.docker.internal:3000
-
-DATALAKE_BASE_URL=http://datalake.$SETUP_CUSTOMER.layernext.ai
-
-#api key secrets
-DATASET_KEY=$DATASET_KEY
-DATASET_SECRET=$DATASET_KEY
-EOL
 
 
-
-# generate studio env
-studio_env="./studio/.env"
-if [ -f $studio_env ]; then
-  echo "Existing env file found for studio. replacing..."
-  echo "Warning: If the system was already built, the system may become non functional due to regeneration of db passwords"
-fi
-cat > $studio_env <<EOL
-#DB
-DB_HOST=host.docker.internal
-DB_PORT=27017
-DB_USER=studio_user
-DB_PASS=$(openssl rand -hex 10)
-DATABASE=studioDB
-
-#DUMP
-DUMP_USER=layernext_dumprestoreuser
-DUMP_USER_PWD=$(openssl rand -hex 10)
-OUTPUT_DIRECTORY=studio
-#change dump keepeing days as prefer and use dump per day like 1,2,3,4,6,12,24
-DUMP_KEEPING_DAYS=30
-DUMP_PER_DAY=4
-
-# Build time
-SETUP_CUSTOMER=$SETUP_CUSTOMER
-MONGODB_ADMIN_PASSWORD=$(openssl rand -hex 16)
-CPU_LIMIT=$CPU_LIMIT
-MEMORY_LIMIT=$MEMORY_LIMIT
-
-#For uploadx listener
-EXPRESS_PORT=8082
-TEAM_ID=6374c3decb468b7a7a68a116
-
-JWT_SECRET = $JWT_SECRET
-ADMIN_EMAIL=$ADMIN_EMAIL
-
-# Auth
-SSO_INTERNAL_SERVER=http://host.docker.internal:8888
-
-PYTHON_BASE_URL=http://host.docker.internal:8081
-BASE_URL=https://studio.$SETUP_CUSTOMER.layernext.ai
-PYTHON_SERVER=../../../contents/uploads/
-FRONT_END_BASE_URL=https://studio.$SETUP_CUSTOMER.layernext.ai
-DATALAKE_INTERNAL_SERVER=http://host.docker.internal:3000
-DATALAKE_BASE_URL=http://host.docker.internal:3000
-# DATALAKE_BASE_URL=https://datalake.$SETUP_CUSTOMER.layernext.ai
-API_URL=https://api.$SETUP_CUSTOMER.layernext.ai
-
-ENVIRONMENT=enterprise
-
-#support email sendgrid
-SUPPORT_EMAIL=$SUPPORT_EMAIL
-SENDGRID_API_KEY=$SENDGRID_API_KEY
-
-# Google login
-GOOGLE_API_KEY=$GOOGLE_API_KEY
-GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID
-GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET
-GOOGLE_REFRESH_TOKEN=$GOOGLE_REFRESH_TOKEN
-
-# Enabling optional features
-RESET_ASSIGNEE_ON_NEW_CYCLE=$RESET_ASSIGNEE_ON_NEW_CYCLE
-
-#api key secrets
-STUDIO_KEY=$STUDIO_KEY
-STUDIO_SECRET=$STUDIO_SECRET
-
-EOL
-
-
-# generate analytics env
-analytics_env="./analytics/.env"
-if [ -f $analytics_env ]; then
-  echo "Existing env file found for analytics. replacing..."
-  echo "Warning: If the system was already built, the system may become non functional due to regeneration of db passwords"
-fi
-cat > $analytics_env <<EOL
-#DB
-DB_HOST=host.docker.internal
-DB_PORT=51017
-DB_USER=analytics_user
-DB_PASS=$(openssl rand -hex 10)
-DATABASE=analyticsDB
-MODEL_DATABASE=modelDB
-
-APP_PORT=5081
-
-#DUMP
-DUMP_USER=layernext_dumprestoreuser
-DUMP_USER_PWD=$(openssl rand -hex 10)
-OUTPUT_DIRECTORY=analytics
-#change dump keepeing days as prefer and use dump per day like 1,2,3,4,6,12,24
-DUMP_KEEPING_DAYS=30
-DUMP_PER_DAY=4
-
-# Build time
-SETUP_CUSTOMER=$SETUP_CUSTOMER
-MONGODB_ADMIN_PASSWORD=$(openssl rand -hex 16)
-
-DB_CPU_LIMIT=$ANALYTICS_DB_CPU_LIMIT
-DB_MEMORY_LIMIT=$ANALYTICS_DB_MEMORY_LIMIT
-
-#Analytics app
-ANALYSIS_CPU_LIMIT=$ANALYTICS_APP_CPU_LIMIT
-ANALYSIS_MEMORY_LIMIT=$ANALYTICS_APP_MEMORY_LIMIT
-
-JWT_SECRET = $JWT_SECRET
-ADMIN_EMAIL=$ADMIN_EMAIL
-
-# Auth
-SSO_INTERNAL_SERVER=http://host.docker.internal:8888
-
-BASE_URL=https://studio.$SETUP_CUSTOMER.layernext.ai
-DATALAKE_INTERNAL_SERVER=http://host.docker.internal:3000
-DATALAKE_BASE_URL=https://datalake.$SETUP_CUSTOMER.layernext.ai
-API_URL=https://api.$SETUP_CUSTOMER.layernext.ai
-
-ENVIRONMENT=enterprise
-TEAM_ID=6374c3decb468b7a7a68a116
-
-# storage
-STORAGE_TYPE=$STORAGE_TYPE
-
-#S3
-AWS_ACCESS_KEY=$AWS_ACCESS_KEY
-AWS_SECRET_KEY=$AWS_SECRET_KEY
-AWS_REGION=$AWS_REGION
-AWS_ROLE=$AWS_ROLE
-
-#analytics
-INSTANCE_TYPE=$INSTANCE_TYPE
-INFERENCE_PLATFORM=$INFERENCE_PLATFORM
-INFERENCE_METHOD=$INFERENCE_METHOD
-
-#api key secrets
-ANALYTICS_KEY=$ANALYTICS_KEY
-ANALYTICS_SECRET=$ANALYTICS_SECRET
-
-EOL
 
 
 # generate ssl creator nginx env
@@ -586,6 +394,4 @@ METALAKE_URL=https://datalake.$SETUP_CUSTOMER.layernext.ai
 API_KEY=$CHAT_KEY
 SECRET_KEY=$CHAT_SECRET
 EOL
-
-pip install python-dotenv
 
