3beaf853d2f08ce118eda68eaa15931cd96028a8
Chathushka <chathushka@zoomi.ca>
2024-03-28 10:19:01 +0530

    Update restore.sh

--- source/manual/restore.sh
+++ source/manual/restore.sh
@@ -6,7 +6,7 @@ set -a  # automatically export all variables
 source /home/ubuntu/layernext-docker-image-deploy/source/manual/.env
 set +a  # stop automatically exporting
 
-mongorestore --drop --port=$CONNECTION_MONGO_PORT --authenticationDatabase=admin --username=$CONNECTION_MONGO_USERNAME --password=$CONNECTION_MONGO_PASSWORD --db=$CONNECTION_DB_NAME --gzip --archive=/home/ubuntu/db-dump/dump/dcchail_dump_`date +%d-%m-%Y`.gz --nsFrom='Dcchail.*' --nsTo='Dcchail.*' --gzip >> $LOG_FILE 2>&1
+mongorestore --drop --port=$CONNECTION_MONGO_PORT --authenticationDatabase=admin --username=$CONNECTION_MONGO_USERNAME --password=$CONNECTION_MONGO_PASSWORD --db=$CONNECTION_DB_NAME --gzip --archive=/home/ubuntu/layernext-docker-image-deploy/source/manual/dump/dcchail_dump_`date +%d-%m-%Y`.gz --nsFrom='Dcchail.*' --nsTo='Dcchail.*' --gzip >> $LOG_FILE 2>&1
 
 
 
