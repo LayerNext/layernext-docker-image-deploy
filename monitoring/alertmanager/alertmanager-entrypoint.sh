3af7d879cd479c12b1be8b1bd01e11cf2598e384
TharinduImalka <tharinduimalka@zoomi.ca>
2024-09-05 18:01:57 +0530

    alert manager issue

--- monitoring/alertmanager/alertmanager-entrypoint.sh
+++ monitoring/alertmanager/alertmanager-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo su
-sudo mkdir -p /etc/alertmanager
+mkdir -p /etc/alertmanager
 chmod 755 /etc/alertmanager
 
 envsubst < /etc/alertmanager/alertmanager.yml > /etc/alertmanager/alertmanager.yml
