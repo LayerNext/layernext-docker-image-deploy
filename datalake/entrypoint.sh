faae11edf741f5a535402050348462e4cb0b591d
TharinduImalka <tharinduimalka@zoomi.ca>
2025-04-03 12:15:42 +0530

    uncomment

--- datalake/entrypoint.sh
+++ datalake/entrypoint.sh
@@ -7,36 +7,36 @@ log_message() {
 
 log_message "Starting container..."
 
-# if [ "$ENABLE_WINDOWS_AUTH" = "true" ]; then
-#     log_message "Windows Authentication is enabled. Setting up Kerberos..."
+if [ "$ENABLE_WINDOWS_AUTH" = "true" ]; then
+    log_message "Windows Authentication is enabled. Setting up Kerberos..."
 
-#     export DEBIAN_FRONTEND=noninteractive
+    export DEBIAN_FRONTEND=noninteractive
 
-#     # Install Kerberos tools if not already installed
-#     apt-get update && apt-get install -y krb5-user || { log_message "Failed to install Kerberos tools.";}
+    # Install Kerberos tools if not already installed
+    apt-get update && apt-get install -y krb5-user || { log_message "Failed to install Kerberos tools.";}
 
-#     # Check if environment variables for Kerberos username and password are set
-#     if [ -z "$KERBEROS_USER" ] || [ -z "$KERBEROS_PASSWORD" ]; then
-#         log_message "Error: Kerberos username or password is not set in environment variables."
-#         exit 1
-#     fi
+    # Check if environment variables for Kerberos username and password are set
+    if [ -z "$KERBEROS_USER" ] || [ -z "$KERBEROS_PASSWORD" ]; then
+        log_message "Error: Kerberos username or password is not set in environment variables."
+        exit 1
+    fi
 
-#     # Check if krb5.conf exists before copying
-#     if [ -f "/app/config/host_krb5/krb5.conf" ]; then
-#         log_message "Found Kerberos configuration..."
-#         cp /app/config/host_krb5/krb5.conf /etc/krb5.conf
-#     else
-#         log_message "Warning: Kerberos config file not found. Skipping copy."
-#     fi
+    # Check if krb5.conf exists before copying
+    if [ -f "/app/config/host_krb5/krb5.conf" ]; then
+        log_message "Found Kerberos configuration..."
+        cp /app/config/host_krb5/krb5.conf /etc/krb5.conf
+    else
+        log_message "Warning: Kerberos config file not found. Skipping copy."
+    fi
 
-#     log_message "Detected Kerberos user: $KERBEROS_USER"
+    log_message "Detected Kerberos user: $KERBEROS_USER"
 
-#     # Set up Kerberos authentication
+    # Set up Kerberos authentication
 
-#     log_message "Kerberos authentication successful."
-# else
-#     log_message "Windows Authentication is disabled. Skipping Kerberos setup."
-# fi
+    log_message "Kerberos authentication successful."
+else
+    log_message "Windows Authentication is disabled. Skipping Kerberos setup."
+fi
 
 #echo a simple message
 log_message "Container started."
