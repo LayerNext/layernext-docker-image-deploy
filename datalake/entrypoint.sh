0638c97cf790303010ee0b314881851fd8a78229
TharinduImalka <tharinduimalka@zoomi.ca>
2025-04-03 12:23:50 +0530

    uodate

--- datalake/entrypoint.sh
+++ datalake/entrypoint.sh
@@ -44,4 +44,3 @@ log_message "Container started."
 
 # Ensure the default command provided in the Dockerfile or Compose file is executed
 log_message "Executing default command..."
-exec python3 app.py
