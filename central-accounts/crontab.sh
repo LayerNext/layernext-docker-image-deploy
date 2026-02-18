73d3575937b56b5f1c355a1cb932286d0d3b2bba
channa-zoomi <channa@zoomi.ca>
2026-02-18 17:06:45 +0530

    added central-accounts

---
+++ central-accounts/crontab.sh
@@ -0,0 +1,5 @@
+PATH_ENV=$(realpath .env)
+PATH_DIR=$(realpath ..)
+#change the path to dump.py
+
+(crontab -l 2>/dev/null; echo "0 * * * * python3 $PATH_DIR/backup/dump.py $PATH_ENV $PATH_DIR") | crontab -
