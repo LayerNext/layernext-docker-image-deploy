6e8bbae0c1b24b6b72b616290094f2b3ce9a10e7
TharinduImalka <tharinduimalka@zoomi.ca>
2026-05-20 11:06:13 +0530

    Refactor email handling in dump.py and update README with correct environment variable names

--- backup/dump.py
+++ backup/dump.py
@@ -24,8 +24,8 @@ AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 DUMP_KEEPING_DAYS = os.getenv("DUMP_KEEPING_DAYS")
 DUMP_PER_DAY = os.getenv("DUMP_PER_DAY")
 SENDER_EMAIL = os.getenv("SUPPORT_EMAIL")
-RECEIVER_EMAILS = ["tharinduimalka@zoomi.ca"]
-COMPANY = os.getenv("COMPANY")
+RECEIVER_EMAILS = [e.strip() for e in os.getenv("RECEIVER_EMAILS").split(",")]
+COMPANY = os.getenv("SETUP_CUSTOMER")
 
 # service database map
 SERVICE_DB_MAP = {
