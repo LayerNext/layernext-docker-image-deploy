42476958df088f2289c9e82523143939efb55d62
TharinduImalka <tharinduimalka@zoomi.ca>
2026-05-20 10:42:14 +0530

    Update crontab script to log alert execution output and change directory before running

--- alert/crontab.sh
+++ alert/crontab.sh
@@ -2,5 +2,4 @@ PATH_ENV=$(realpath .env)
 PATH_DIR=$(realpath ..)
 VENV_PYTHON="$PATH_DIR/venv/bin/python3"
 
-# Run alert every 5 minutes (adjust schedule as needed)
-(crontab -l 2>/dev/null; echo "*/5 * * * * $VENV_PYTHON $PATH_DIR/alert/alert.py") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * cd $PATH_DIR/alert && $VENV_PYTHON $PATH_DIR/alert/alert.py >> $PATH_DIR/alert/alert.log 2>&1") | crontab -
